6b10eac3a14be7a4b4b546bfd124f682cbb2622e
ZLW07 <205430509@qq.com>
2023-08-06 22:28:42 +0800

    ground

--- +MR/MatrixExp6.m
+++ +MR/MatrixExp6.m
@@ -19,13 +19,13 @@ function T = MatrixExp6(se3mat)
 %         0    1.0000    0.0000    3.0000
 %         0         0         0    1.0000 
 
-omgtheta = so3ToVec(se3mat(1: 3, 1: 3));
-if NearZero(norm(omgtheta))
+omgtheta = MR.so3ToVec(se3mat(1: 3, 1: 3));
+if MR.NearZero(norm(omgtheta))
     T = [eye(3), se3mat(1: 3, 4); 0, 0, 0, 1];
 else
-    [omghat, theta] = AxisAng3(omgtheta);
+    [omghat, theta] = MR.AxisAng3(omgtheta);
     omgmat = se3mat(1: 3, 1: 3) / theta; 
-    T = [MatrixExp3(se3mat(1: 3, 1: 3)), ...
+    T = [MR.MatrixExp3(se3mat(1: 3, 1: 3)), ...
          (eye(3) * theta + (1 - cos(theta)) * omgmat ...
           + (theta - sin(theta)) * omgmat * omgmat) ...
             * se3mat(1: 3, 4) / theta;
