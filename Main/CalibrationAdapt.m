6ba5ab84a802e9ee0a3b964aa76c53beb8c31fff
wei.zhang07 <wei.zhang07@uih-surgical.com>
2023-09-25 20:22:32 +0800

    calibration adaptor

--- Main/CalibrationAdapt.m
+++ Main/CalibrationAdapt.m
@@ -1,9 +1,9 @@
 clc
 clear
 addpath("Main\","Data\CalibrationData\Adaptor\")
-fid = fopen('Cyl.txt');
-Cylinder_Point = zeros(75,3);
-Cylinder_Axis = zeros(75,3);
+fid = fopen('axis.txt');
+Cylinder_Point = [];
+Cylinder_Axis = [];
 ii = 1;
 tmp = zeros(3,3);
 while ~feof(fid)
@@ -15,36 +15,49 @@ while ~feof(fid)
     end
     ii = ii+1;
 end
-scatter3(Cylinder_Point(1:6,1),Cylinder_Point(1:6,2), Cylinder_Point(1:6,3));
 fclose(fid);
 
-fid = fopen('plane.txt');
-Plane_Point = zeros(75,3);
-Plane_Axis = zeros(75,3);
+fid = fopen('plane_xy.txt');
+Plane_Point_tmp = [];
+Plane_Axis_tmp = [];
 ii = 1;
 while ~feof(fid)
     for ij = 1:1:3
         str = fgetl(fid);
         value = sscanf(str,'%f %f');
-         Plane_Point(ii,ij) =value(1);
-         Plane_Axis(ii,ij) =value(2);
+         Plane_Point_tmp(ii,ij) =value(1);
+         Plane_Axis_tmp(ii,ij) =value(2);
     end
     ii = ii+1;
 end
 hold on
-scatter3(Plane_Point(1:6,1),Plane_Point(1:6,2), Plane_Point(1:6,3));
 fclose(fid);
 
-Point = zeros(75,3);
-for ji = 1:75
-    vpt = Plane_Axis(ji,1)*Cylinder_Axis(ji,1) + Plane_Axis(ji,2)*Cylinder_Axis(ji,2) + Plane_Axis(ji,3)*Cylinder_Axis(ji,3);
-    t = ((Plane_Point(ji,1) - Cylinder_Point(ji,1)*Plane_Axis(ji,1)) ...
-    + (Plane_Point(ji,2) - Cylinder_Point(ji,2)*Plane_Axis(ji,2)) ...
-    + (Plane_Point(ji,3) - Cylinder_Point(ji,3)*Plane_Axis(ji,3)))/vpt;
-    Point(ji,1) = Cylinder_Point(ji,1) + Cylinder_Axis(ji,1)*t;
-    Point(ji,2) = Cylinder_Point(ji,2) + Cylinder_Axis(ji,2)*t;
-    Point(ji,3) = Cylinder_Point(ji,3) + Cylinder_Axis(ji,3)*t;
+Plane_Point = [];
+Plane_Axis = [];
+for id = 1:6: length(Plane_Point_tmp)
+    for di = 0:2
+        Plane_Point= [Plane_Point;Plane_Point_tmp(id + di,:)];
+        Plane_Axis= [Plane_Axis;Plane_Axis_tmp(id + di,:)];
+    end
 end
+
+Point = [];
+index = 1;
+for jii = 1:3
+    for ji = 1:2
+        vpt = Plane_Axis(jii,1)*Cylinder_Axis(ji,1) + Plane_Axis(jii,2)*Cylinder_Axis(ji,2) + Plane_Axis(jii,3)*Cylinder_Axis(ji,3);
+        t = ((Plane_Point(jii,1) - Cylinder_Point(ji,1)*Plane_Axis(jii,1)) ...
+        + (Plane_Point(jii,2) - Cylinder_Point(ji,2)*Plane_Axis(jii,2)) ...
+        + (Plane_Point(jii,3) - Cylinder_Point(ji,3)*Plane_Axis(jii,3)))/vpt;
+
+        Point(index,1) = Cylinder_Point(ji,1) + Cylinder_Axis(ji,1)*t;
+        Point(index,2) = Cylinder_Point(ji,2) + Cylinder_Axis(ji,2)*t;
+        Point(index,3) = Cylinder_Point(ji,3) + Cylinder_Axis(ji,3)*t;
+        index = index+1;
+    end
+end
+
 
 
 
