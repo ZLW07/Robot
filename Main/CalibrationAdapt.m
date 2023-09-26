0ca712d08db4b13a8705665d1ad2b8eef71a5223
wei.zhang07 <wei.zhang07@uih-surgical.com>
2023-09-26 15:05:12 +0800

    calibration adaptor

--- Main/CalibrationAdapt.m
+++ Main/CalibrationAdapt.m
@@ -1,7 +1,8 @@
 clc
 clear
-addpath("Main\","Data\CalibrationData\Adaptor\")
+addpath("Main\","Data\CalibrationData\Adaptor\",'StuabliRobot\')
 fid = fopen('axis.txt');
+%% Cylinder axis and point 
 Cylinder_Point = [];
 Cylinder_Axis = [];
 ii = 1;
@@ -17,6 +18,7 @@ while ~feof(fid)
 end
 fclose(fid);
 
+%% plane axis and point 
 fid = fopen('plane_xy.txt');
 Plane_Point_tmp = [];
 Plane_Axis_tmp = [];
@@ -42,6 +44,8 @@ for id = 1:6: length(Plane_Point_tmp)
     end
 end
 
+
+%% Point of intersection
 Point = [];
 index = 1;
 for number = 0: length(Plane_Point)/3 -1
@@ -60,6 +64,56 @@ for number = 0: length(Plane_Point)/3 -1
         end
     end
 end
+
+joint_num = [];
+num = 0;
+points = [];
+for iIndex = 1:6:length(Point)
+    num = num +1;
+    tmpPoint = [];
+    sub1 = norm(Point(iIndex,:) - Point(iIndex+2,:),2);
+    sub2 = norm(Point(iIndex,:) - Point(iIndex+4,:),2);
+    sub3 = norm(Point(iIndex +2,:) - Point(iIndex+4,:),2);
+    if((sub1 < 0.5))
+          tmpPoint = [tmpPoint;Point(iIndex,:)];
+          tmpPoint = [tmpPoint;Point(iIndex+1,:)];
+          tmpPoint = [tmpPoint;Point(iIndex+2,:)];
+          tmpPoint = [tmpPoint;Point(iIndex+3,:)];
+          if((sub3 < 0.5))
+            tmpPoint = [tmpPoint;Point(iIndex+4,:)];
+            tmpPoint = [tmpPoint;Point(iIndex+5,:)];
+          end
+          joint_num = [joint_num;num];
+    else
+        if((sub2 < 0.5))
+          tmpPoint = [tmpPoint;Point(iIndex,:)];
+          tmpPoint = [tmpPoint;Point(iIndex+1,:)];
+          tmpPoint = [tmpPoint;Point(iIndex+4,:)];
+          tmpPoint = [tmpPoint;Point(iIndex+5,:)];
+          joint_num = [joint_num;num];
+        else
+            if(sub3 <0.5)
+                tmpPoint = [tmpPoint;Point(iIndex+2,:)];
+                tmpPoint = [tmpPoint;Point(iIndex+3,:)];
+                tmpPoint = [tmpPoint;Point(iIndex+4,:)];
+                tmpPoint = [tmpPoint;Point(iIndex+5,:)];
+                joint_num = [joint_num;num];
+            end 
+        end
+    end
+    [m,n] = size(tmpPoint);
+    if(m>2)
+        data = [0,0,0];
+         for ii = 1: m
+          data = data + tmpPoint(ii,:);
+        end
+        data =data/m;
+        points = [points;data];
+    end
+end
+
+
+
 
 
 
